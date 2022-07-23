51e41dd35c8a13023dfd55516d54a9aabb354fbf
Victor Westerhuis <victor@westerhu.is>
2022-07-23 21:39:19 +0200

    Allow quoting to preserve spaces in the login option in config file
    There are three ways to quote supported, derived from shell quoting.
    
    - Without surrounding quotes, backspace unconditionally escapes the
      following character. `\ ` is parsed as a single space which does not
      separate words.
    - Within double quotes, backspace only escapes backspace and double
      quotes. `"\\"` is parsed as a single backslash, while `"\b"` is parsed
      as the two characters backslash and 'b'. All white space is preserved
      within quotes.
    - Within single quotes, backspace does not work as an escape character.
      `'\"'` is parsed as the two characters backslash and dobule quote.
      All white space is preserved within quotes.
    
    Fixes #57

--- tests/test_shl.c
+++ tests/test_shl.c
@@ -24,8 +24,149 @@
  */
 
 #include "test_common.h"
+#include "shl_misc.h"
+
+#define check_assert_string_list_eq(X, Y) \
+	do { \
+		unsigned int i; \
+		const char **x, **y; \
+	\
+		x = (X); \
+		y = (Y); \
+	\
+		for (i = 0; x[i] && y[i]; ++i) \
+			ck_assert_str_eq(x[i], y[i]); \
+		ck_assert_ptr_eq(x[i], NULL); \
+		ck_assert_ptr_eq(y[i], NULL); \
+	} while (0)
+
+START_TEST(test_split_command_string)
+{
+	int ret;
+	unsigned int i, n, n_list, n_expected;
+	char **list;
+
+	const char *invalid_command_strings[] = {
+		"\"", "'", "\\",
+		"\"/bin/true", "'/bin/true", "/bin/true\\",
+		"ls -h \"*.c'",
+	};
+	n = sizeof(invalid_command_strings) / sizeof(invalid_command_strings[0]);
+
+	for (i = 0; i < n; ++i) {
+		list = TEST_INVALID_PTR;
+		n_list = -10;
+
+		ret = shl_split_command_string(invalid_command_strings[i],
+					       &list, &n_list);
+		ck_assert_int_eq(ret, -EINVAL);
+		ck_assert_ptr_eq(list, TEST_INVALID_PTR);
+		ck_assert_uint_eq(n_list, (unsigned int)-10);
+	}
+
+	const char *expected_command_list[] = {
+		"'/bin/command with space",
+		"\t\\argument=\"quoted\"",
+		"plain\3argument",
+		" an\tother='ere",
+		"\"ends with \\",
+		"\\\"more\\bquotes\\",
+		NULL
+	};
+	n_expected = sizeof(expected_command_list) /
+	  sizeof(expected_command_list[0]) - 1;
+	const char *valid_command_strings[] = {
+		"\\'/bin/command\\ with\\ space \\\t\\\\argument=\\\"quoted\\\" plain\3argument \\ an\\\tother=\\'ere \\\"ends\\ with\\ \\\\ \\\\\\\"more\\\\bquotes\\\\",
+		"\"'/bin/command with space\" \"\t\\argument=\\\"quoted\\\"\" \"plain\3argument\" \" an\tother='ere\" \"\\\"ends with \\\\\" \"\\\\\\\"more\\bquotes\\\\\"",
+		"\"'\"'/bin/command with space' '\t\\argument=\"quoted\"' 'plain\3argument' ' an\tother='\"'\"'ere' '\"ends with \\' '\\\"more\\bquotes\\'",
+		"   \\'/bin/command\\ with\\ space\t\t\\\t\\\\argument=\\\"quoted\\\"\t plain\3argument  \t\\ an\\\tother=\\'ere   \\\"ends\\ with\\ \\\\   \t  \\\\\\\"more\\\\\\bquotes\\\\   \t \t",
+	};
+	n = sizeof(valid_command_strings) / sizeof(valid_command_strings[0]);
+
+	for (i = 0; i < n; ++i) {
+		list = TEST_INVALID_PTR;
+		n_list = -10;
+
+		ret = shl_split_command_string(valid_command_strings[i], &list,
+					       &n_list);
+		ck_assert_int_eq(ret, 0);
+		ck_assert_ptr_ne(list, TEST_INVALID_PTR);
+		check_assert_string_list_eq((const char**)list, expected_command_list);
+		ck_assert_uint_eq(n_list, n_expected);
+	}
+
+	const char *empty_command_strings[] = {
+		"",
+		" ",
+		"\t\t  \t",
+	};
+	n = sizeof(empty_command_strings) / sizeof(empty_command_strings[0]);
+
+	for (i = 0; i < n; ++i) {
+		list = TEST_INVALID_PTR;
+		n_list = -10;
+
+		ret = shl_split_command_string(empty_command_strings[i], &list,
+					       &n_list);
+		ck_assert_int_eq(ret, 0);
+		ck_assert_ptr_ne(list, TEST_INVALID_PTR);
+		ck_assert_ptr_eq(list[0], NULL);
+		ck_assert_uint_eq(n_list, 0);
+	}
+
+	{
+		list = TEST_INVALID_PTR;
+		n_list = -10;
+
+		ret = shl_split_command_string(valid_command_strings[0], &list,
+					       &n_list);
+		ck_assert_int_eq(ret, 0);
+		ck_assert_ptr_ne(list, TEST_INVALID_PTR);
+		check_assert_string_list_eq((const char**)list, expected_command_list);
+		ck_assert_uint_eq(n_list, n_expected);
+	}
+
+	{
+		list = TEST_INVALID_PTR;
+
+		ret = shl_split_command_string(valid_command_strings[0], &list,
+					       NULL);
+		ck_assert_int_eq(ret, 0);
+		ck_assert_ptr_ne(list, TEST_INVALID_PTR);
+		check_assert_string_list_eq((const char**)list, expected_command_list);
+	}
+
+	{
+		n_list = -10;
+
+		ret = shl_split_command_string(valid_command_strings[0], NULL,
+					       &n_list);
+		ck_assert_int_eq(ret, -EINVAL);
+		ck_assert_uint_eq(n_list, (unsigned int)-10);
+	}
+
+	{
+		list = TEST_INVALID_PTR;
+		n_list = -10;
+
+		ret = shl_split_command_string(NULL, &list, &n_list);
+		ck_assert_int_eq(ret, -EINVAL);
+		ck_assert_ptr_eq(list, TEST_INVALID_PTR);
+		ck_assert_uint_eq(n_list, (unsigned int)-10);
+	}
+
+	{
+		n_list = -10;
+
+		ret = shl_split_command_string(NULL, NULL, &n_list);
+		ck_assert_int_eq(ret, -EINVAL);
+		ck_assert_uint_eq(n_list, (unsigned int)-10);
+	}
+}
+END_TEST
 
 TEST_DEFINE_CASE(misc)
+	TEST(test_split_command_string)
 TEST_END_CASE
 
 TEST_DEFINE(
